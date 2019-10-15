d66b60ca3b08c5b968a6fa5852ec5ac2f94936bc
Nick Leffler <nicholas.leffler@mnxsolutions.com>
2019-10-15 14:18:16 -0400

    vvv

--- checkWPdeploy.sh
+++ checkWPdeploy.sh
@@ -7,4 +7,6 @@ if [[ $(mdata-get WPinstall) == "yes" ]]; then
 	curl -s -o 'wpDeploy.sh' 'https://raw.githubusercontent.com/mnxnick2/installWPnick/master/wpDeploy.sh'
 	chmod './wpDeploy.sh'
 	bash -x './wpDeploy.sh'
+else
+	echo "mdata to WPinstall not set to yes"
 fi
