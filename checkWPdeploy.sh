bc58d999ae0f08104899ee1d9d549a42ea0de784
Nick Leffler <nicholas.leffler@mnxsolutions.com>
2019-10-15 14:19:19 -0400

    vvv

--- checkWPdeploy.sh
+++ checkWPdeploy.sh
@@ -2,11 +2,11 @@
 # FIle should be added here to the below location
 # /var/zoneinit/includes/30-checkWPdeploy.sh
 
-if [[ $(mdata-get WPinstall) == "yes" ]]; then
+if [[ $(mdata-get MWPinstall) == "yes" ]]; then
 	cd /root/
 	curl -s -o 'wpDeploy.sh' 'https://raw.githubusercontent.com/mnxnick2/installWPnick/master/wpDeploy.sh'
 	chmod './wpDeploy.sh'
 	bash -x './wpDeploy.sh'
 else
-	echo "mdata to WPinstall not set to yes"
+	echo "mdata to MWPinstall not set to yes"
 fi
