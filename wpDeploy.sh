45641d608187af19d1cdc74b03f8f43727a70106
Nick Leffler <nicholas.leffler@mnxsolutions.com>
2019-11-19 10:48:34 -0500

    added start time

--- wpDeploy.sh
+++ wpDeploy.sh
@@ -35,6 +35,7 @@ mdata-put wpadmin_password ${wpapasswd}
 mdata-put root_SQL_password ${sqlpswd}
 mdata-put wp_SQL_password ${wpasswd}
 mdata-put full_URL ${fullURL}
+mdata-put start_time ${startTime}
 mdata-put done_time $(date +'%Y%m%d_%H%M%S')
 }
 
@@ -325,6 +326,9 @@ pkgin -y in php73-zip
 #                           Starts HERE                                #
 #                                                                      #
 ########################################################################
+
+#Get start time
+startTime=$(date +'%Y%m%d_%H%M%S')
 
 # set defaults
 siteProto="http://"
